adc2994b75fc1aa4937d0dc99f376593d671bc79
Animesh <animeshyesugade2002@gmail.com>
2023-09-21 11:59:54 +0530

    Day 18 spot painting

---
+++ Day-18/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle,Screen
+import turtle
+from random import choice,randint
+# timmy = Turtle()
+# my_screen = Screen()
+# # timmy.shape('turtle')
+# timmy.color('magenta')
+# turtle.colormode(255)
+#
+# side = 3
+# colors = ['blue','medium spring green','orange red','blue violet','medium violet red','red',
+#           'yellow']
+# angles = [0,90,180,270]
+# timmy.pensize(10)
+# timmy.speed('fastest')
+# for i in range(8):
+#     timmy.color(choice(colors))
+#     for j in range(side):
+#         timmy.forward(100)
+#         timmy.left(360 / side)
+#     side += 1
+# my_screen.exitonclick()
+# angle = 0
+# while angle < 360:
+#     tup = (randint(0,255),randint(0,255),randint(0,255))
+#     timmy.pencolor(tup)
+#     timmy.circle(100)
+#     timmy.setheading(angle)
+#     angle += 5
+#
+#
+# my_screen.exitonclick()
